5c2ffd8fe92e3b6f20504f57be56e85c7653bec4
朱吉达 <402447767@qq.com>
2018-04-08 16:06:28 +0800

    Try
    创建工程

---
+++ HolleWordDemo/HolleWordDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HolleWordDemo
+//
+//  Created by 朱吉达 on 2018/4/8.
+//  Copyright © 2018年 朱吉达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
